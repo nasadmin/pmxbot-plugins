92863f093bd856d6210c3ebebf3f6b0cff3864fd
Chris Jowett <cryptk@gmail.com>
2012-10-24 06:41:32 +0000

    Adding infobot-esque factoid plugin Signed-off-by: Chris Jowett <cryptk@gmail.com>

---
+++ factoid/pmxbot_factoid/factoid.py
@@ -0,0 +1,73 @@
+# -*- coding: utf-8 -*-
+from __future__ import absolute_import
+
+import re
+
+import pmxbot
+from pmxbot.core import command
+from pmxbot import storage
+
+class Factoid(storage.SelectableStorage):
+
+    @classmethod
+    def initialize(cls):
+        cls.store = cls.from_URI(pmxbot.config.database)
+        cls._finalizers.append(cls.finalize)
+
+    @classmethod
+    def finalize(cls):
+        del cls.store
+
+class SQLiteFactoid(Factoid, storage.SQLiteStorage):
+    def init_tables(self):
+        query = "CREATE TABLE IF NOT EXISTS factoids (key varchar, factoid varchar, PRIMARY KEY (key));"
+        self.db.execute(query)
+        self.db.commit()
+
+    def get_factoid(self, key):
+        query = "SELECT factoid FROM factoids WHERE key = ?"
+        result = self.db.execute(query, [key]).fetchall()
+        print result[0][0]
+        if len(result) > 0:
+            return result[0][0]
+        else:
+            return "I don't have a factoid for %s" % key
+
+    def set_factoid(self, key, factoid):
+        query = "INSERT INTO factoids (key, factoid) values (?, ?)"
+        try:
+           self.db.execute(query, [key, factoid])
+        except self.db.IntegrityError:
+            currFactoid = self.get_factoid(key)
+            return "But %s is already %s (perhaps try !factoid replace:)" % (key, currFactoid)
+        return "Factoid Added!"
+
+    def update_factoid(self, key, factoid):
+        query = "INSERT OR REPLACE INTO factoids (key, factoid) values (?, ?)"
+        try:
+           self.db.execute(query, [key, factoid])
+        except self.db.IntegrityError:
+            factoid = self.get_factoid(key)
+            return "But %s is already %s (perhaps try !factoid replace:)" % (result[0], result[1])
+        return "Factoid Added!"
+
+
+@command("factoid", aliases=("f",), doc='If passed with nothing then get a '
+    'random factoid. If passed with some string then return that factoid.'
+    'If prepended with "add:" then add it to the db, eg "!factoid add: foo is bar'
+    'If prepended with replace: then replace an existing factoid')
+def FactoidCmd(client, event, channel, nick, rest):
+    rest = rest.strip()
+    if rest.startswith('add: '):
+        cmdString = rest.split(' ', 1)[1]
+        m = re.match("(?P<key>.*) is (?P<factoid>.*)", cmdString)
+        if m:
+            return Factoid.store.set_factoid(m.group('key').strip(), m.group('factoid').strip())
+        return "Sorry, I didn't quite catch that... try !factoid add: something is something else"
+    if rest.startswith('replace: '):
+        cmdString = rest.split(' ', 1)[1]
+        m = re.match("(?P<key>.*) is (?P<factoid>.*)", cmdString)
+        if m:
+            return Factoid.store.update_factoid(m.group('key').strip(), m.group('factoid').strip())
+        return "Sorry, I didn't quite catch that... try !factoid replace: something is something else"
+    return "%s is %s" % (rest, Factoid.store.get_factoid(rest))
---
+++ factoid/setup.py
@@ -0,0 +1,31 @@
+import setuptools
+
+setup_params = dict(
+    name="pmxbot-factoid",
+    version = "0.1",
+    packages=setuptools.find_packages(),
+    entry_points=dict(
+        pmxbot_handlers = [
+            'factoids = pmxbot_factoid.factoid:Factoid.initialize',
+        ]
+    ),
+    description="Store and retrieve factoids similar to infobot",
+    license = 'MIT',
+    author="Chris Jowett",
+    author_email="cryptk@gmail.com",
+    maintainer = 'Chris Jowett',
+    maintainer_email = 'cryptk@gmail.com',
+    url = 'https://www.cryptkcoding.com',
+    classifiers=[
+        'Development Status :: 4 - Beta',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: POSIX',
+        'Programming Language :: Python :: 2.6',
+        'Programming Language :: Python :: 2.7',
+        'Topic :: Communications :: Chat',
+        'Topic :: Communications :: Chat :: Internet Relay Chat',
+    ],
+)
+
+if __name__ == '__main__':
+    setuptools.setup(**setup_params)
