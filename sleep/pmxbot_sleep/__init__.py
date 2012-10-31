915f1613a3769b8fa33cef9fac491cf2fc9c6448
Tom <ka6sox@gmail.com>
2012-10-30 18:10:57 -0700

    fix typos

--- sleep/pmxbot_sleep/__init__.py
+++ sleep/pmxbot_sleep/__init__.py
@@ -5,7 +5,7 @@ import random
 
 @command("sleep", aliases=(), doc="Sleep enforcer for Coders beyond their useful amount of time to code")
 def sleep(client, event, channel, nick, rest):
-    sleep = ['uses the banhammer to put WHO to sleep for the next 12 hours',
+    sleep = ['uses the banhammer to put WHO to sleep for the next 12 hours']
 
     if rest:
         target = rest
